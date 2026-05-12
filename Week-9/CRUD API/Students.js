9254481d35ed9b26f603d8e40c0f3b61307db195
Anoushka210 <anoush2510@gmail.com>
2026-05-12 21:07:47 +0530

    a backend application with REST API to perform CRUD operations on student data

---
+++ Week-9/CRUD API/Students.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose"); 
+const studentSchema = new mongoose.Schema({ 
+    rollNo: { type: String, required: true, unique: true }, 
+    name: String, 
+    className: String 
+}); 
+
+module.exports = mongoose.model("Student", studentSchema);
